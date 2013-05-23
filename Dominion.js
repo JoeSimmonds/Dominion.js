a8468b8911cf379a229b620ecef701926b381b20
Joe Simmonds <joe.simmonds@gmail.com>
2013-05-23 11:08:43 +0100

    Appending text boxes

--- Dominion.js
+++ Dominion.js
@@ -3,6 +3,13 @@ var Dominion = Dominion || {};
 
 (function($) {
 	$.fn.Dominion = {};
+	
+	//adds a text box to each element in the list
 	$.fn.Dominion.AppendTextBox = function(){
+				return this.each(function(){
+				var depositInput = $(document.createElement("input"));
+				depositInput.attr("type", "text");
+				$(this).append(depositInput);
+			}
 	}
 })(jQuery);
