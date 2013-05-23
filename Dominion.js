cfd637e06860119bf43509c00a1c0398b02d76dd
Joe Simmonds <joe.simmonds@gmail.com>
2013-05-23 12:14:39 +0100

    Can add select boxes now. Also refactored to make functions more private

--- Dominion.js
+++ Dominion.js
@@ -7,18 +7,44 @@
 var Dominion = Dominion || {};
 Dominion.FunctionNames = Dominion.FunctionNames || {};
 Dominion.FunctionNames.AppendTextBox = "AppendTextBox";
+Dominion.FunctionNames.AppendSelect = "AppendSelect";
+Dominion.FunctionNames.AppendElement = "AppendElement";
 
 (function ($) {
 	$.fn.Dominion = {};
 	
 	//adds a text box to each element in the list
 	$.fn.Dominion = function (functionName) {
-	    if (functionName === Dominion.functionNames.AppendTextBox) {
-	        return this.each(function() {
-	            var depositInput = $(document.createElement("input"));
-	            depositInput.attr("type", "text");
-	            $(this).append(depositInput);
-	        });
-	    }
+	    if (functionName === Dominion.FunctionNames.AppendTextBox) {
+	        return appendTextBox.apply(this);
+	    } else if (functionName === Dominion.FunctionNames.AppendSelect) {
+	        return appendSelect.apply(this);
+	    };
 	};
+
+    //options looks like 
+    //{elementName:"input"
+    // attributes:{attribute1Name:"attribute1Value", attribute2Name, "attribute2Value"}
+	function appendElement(options) {
+	    return this.each(function () {
+	        var el = $(document.createElement(options.elementName));
+	        for (var attributeName in options.attributes) {
+	            el.attr(attributeName, options.attributes[attributeName]);
+	        }
+
+	        $(this).append(el);
+	    });
+	};
+
+	function appendTextBox() {
+	    return appendElement.apply(this, [{
+	        elementName: "input",
+	        attributes: { type: "text" }
+	    }]);
+	};
+
+	function appendSelect() {
+	    return appendElement.apply(this, [{ elementName: "select" }]);
+	};
+
 })(jQuery);
