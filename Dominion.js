f0b6fb48f818a80d819c87bbd334e11554b24f0c
Joe Simmonds <joe.simmonds@gmail.com>
2013-05-23 11:29:46 +0100

    Can append a text box

--- Dominion.js
+++ Dominion.js
@@ -1,15 +1,24 @@
 //Dominion.js
-var Dominion = Dominion || {};
+//Items to do
+//============
+// a function to configure that allows the promotion of text based function names to first class jQuery plugin functions.
 
-(function($) {
+
+var Dominion = Dominion || {};
+Dominion.FunctionNames = Dominion.FunctionNames || {};
+Dominion.FunctionNames.AppendTextBox = "AppendTextBox";
+
+(function ($) {
 	$.fn.Dominion = {};
 	
 	//adds a text box to each element in the list
-	$.fn.Dominion.AppendTextBox = function(){
-				return this.each(function(){
-				var depositInput = $(document.createElement("input"));
-				depositInput.attr("type", "text");
-				$(this).append(depositInput);
-			}
-	}
+	$.fn.Dominion = function (functionName) {
+	    if (functionName === Dominion.functionNames.AppendTextBox) {
+	        return this.each(function() {
+	            var depositInput = $(document.createElement("input"));
+	            depositInput.attr("type", "text");
+	            $(this).append(depositInput);
+	        });
+	    }
+	};
 })(jQuery);
