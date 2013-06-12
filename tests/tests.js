ed61f4ff3154a12cf9b811c0c81d66d3d0c1aa03
Joe Simmonds <joe.simmonds@gmail.com>
2013-06-12 22:47:24 +0100

    First few tests

--- tests/tests.js
+++ tests/tests.js
@@ -1,3 +1,70 @@
 test( "hello test", function() {
   ok( 1 == "1", "Passed!" );
 });
+
+test("Can Append A Div", function(){
+    var $fixture = $("#qunit-fixture");  
+    $fixture.Dominion("appendDiv");
+    equal($("div", $fixture).length,1,"A div was created");
+});
+
+test("Can Append A Span", function(){
+    var $fixture = $("#qunit-fixture");
+    $fixture.Dominion("appendSpan");
+    equal($("span", $fixture).length,1,"A span was created");
+});
+
+test("Can Append A Line Break", function(){
+    var $fixture = $("#qunit-fixture");
+    $fixture.Dominion("appendLineBreak");
+    equal($("br", $fixture).length,1,"A br tag was created");
+});
+
+test("Can Append A Text Box", function(){
+    var $fixture = $("#qunit-fixture");
+    $fixture.Dominion("appendTextBox");
+    equal($("input[type='text']", $fixture).length,1,"A text box was created");
+});
+
+test("Can Append A Select Box", function(){
+    var $fixture = $("#qunit-fixture");
+    $fixture.Dominion("appendSelect");
+    equal($("select", $fixture).length,1,"A Select Box was created");
+});
+
+test("Can Append options", function(){
+    var $fixture = $("#qunit-fixture");
+    $fixture.Dominion("appendOption");
+    equal($("Option", $fixture).length,1,"An option was created");
+});
+
+test("Can Append An Arbitrary Element", function(){
+    var $fixture = $("#qunit-fixture");
+    $fixture.Dominion("appendElement",{elementName:"test"});
+    equal($("test", $fixture).length,1,"An test element was created");
+});
+
+test ("Can Set The Element's Id",function(){
+    var $fixture = $("#qunit-fixture");  
+    $fixture.Dominion("appendDiv",{id:"testId"});
+    equal($("div#testId", $fixture).length,1,"The Id was set correctly");
+});
+
+test ("Can Set The Element's Text",function(){
+    var $fixture = $("#qunit-fixture");
+    var testText="here is some test text";
+    $fixture.Dominion("appendDiv",{text:testText});
+    equal($("div", $fixture).text(),testText,"The Text was set correctly");
+});
+
+test ("Can Set Attributes On The Element",function(){    
+    var $fixture = $("#qunit-fixture");
+    $fixture.Dominion("appendDiv",{attributes:{att1:"testValue"}});
+    equal($("div[att1='testValue']", $fixture).length,1,"The attribute was set correctly");
+});
+
+test ("Can Set cssClass On The Element",function(){
+    var $fixture = $("#qunit-fixture");
+    $fixture.Dominion("appendDiv",{cssClass:"testClass"});
+    equal($("div.testClass", $fixture).length,1,"The class was set correctly");
+});
